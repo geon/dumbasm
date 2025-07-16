db942eff93abbdd61b9007d49d64737ce7d66d80
Victor Widell <victor.widell@divid.se>
2025-07-16 22:42:07 +0200

    Refactor. Moved base to argument.

--- src/parsers/parseNumber.ts
+++ src/parsers/parseNumber.ts
@@ -3,11 +3,9 @@ import { parseMonad } from "./combinators/parseMonad.js";
 import { parseOneOrMore } from "./combinators/parseSome.js";
 import type { Parser } from "./combinators/Parser.js";
 
-export const parseNumber = parseDigits();
+export const parseNumber = parseDigits(10);
 
-function parseDigits(): Parser<number> {
-	const base = 10;
-
+function parseDigits(base: number): Parser<number> {
 	return parseMonad(parseOneOrMore(parseDigit(base)), (digits) =>
 		parseInt(digits.join(""), base),
 	);
