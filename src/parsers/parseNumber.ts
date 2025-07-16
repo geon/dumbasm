ad61bc3903acbe4d5a1c3076c1fa1c7e4afe3543
Victor Widell <victor.widell@divid.se>
2025-07-16 22:40:08 +0200

    Refactor. Extracted function parseDigits.

--- src/parsers/parseNumber.ts
+++ src/parsers/parseNumber.ts
@@ -3,7 +3,10 @@ import { parseMonad } from "./combinators/parseMonad.js";
 import { parseOneOrMore } from "./combinators/parseSome.js";
 import type { Parser } from "./combinators/Parser.js";
 
-export const parseNumber: Parser<number> = parseMonad(
-	parseOneOrMore(parseDigit(10)),
-	(digits) => parseInt(digits.join(""), 10),
-);
+export const parseNumber = parseDigits();
+
+function parseDigits(): Parser<number> {
+	return parseMonad(parseOneOrMore(parseDigit(10)), (digits) =>
+		parseInt(digits.join(""), 10),
+	);
+}
