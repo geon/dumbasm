35c04437396e126c9e8d2071b5ac9a8ddd4c342b
Victor Widell <victor.widell@divid.se>
2025-07-16 22:17:44 +0200

    Test: parseOneOrMore > no match

--- src/parsers/combinators/tests/parseOneOrMore.test.ts
+++ src/parsers/combinators/tests/parseOneOrMore.test.ts
@@ -12,4 +12,10 @@ testExamples("parseOneOrMore", [
 			parsed: ["a", "a"],
 		},
 	},
+	{
+		name: "no match",
+		parser: parseOneOrMore(parseChar("b")),
+		input: "abc",
+		result: undefined,
+	},
 ]);
