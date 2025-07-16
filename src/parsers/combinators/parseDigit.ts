f4ca0447e88ca7624afeee4be3b519097ab6bee2
Victor Widell <victor.widell@divid.se>
2025-07-16 21:45:19 +0200

    Test: parseDigitDec

---
+++ src/parsers/combinators/parseDigit.ts
@@ -0,0 +1,15 @@
+import { parseAlternatives } from "./parseAlternatives.js";
+import { parseChar } from "./parseChar.js";
+import type { Parser } from "./Parser.js";
+
+const digits = [
+	[...Array(10)].map((_, x) => [String.fromCharCode("0".charCodeAt(0) + x)]),
+	[...Array(26)].map((_, x) => [
+		String.fromCharCode("a".charCodeAt(0) + x),
+		String.fromCharCode("A".charCodeAt(0) + x),
+	]),
+].flat();
+
+export function parseDigit(base: number): Parser<string> {
+	return parseAlternatives(digits.slice(0, base).flat().map(parseChar));
+}
