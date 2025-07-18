fab76345d5d9102a815e0101bb6ead05b941c5c4
Victor Widell <victor.widell@divid.se>
2025-07-18 19:43:06 +0200

    Test: parseNumber > 123

--- src/parsers/parseNumber.ts
+++ src/parsers/parseNumber.ts
@@ -1,7 +1,17 @@
+import { parseDigit } from "./combinators/parseDigit.js";
+import { parseMonad } from "./combinators/parseMonad.js";
+import { parseOneOrMore } from "./combinators/parseOneOrMore.js";
 import type { Parser } from "./combinators/Parser.js";
 
-export type ParsedNumber = never;
-
-export const parseNumber: Parser<ParsedNumber> = (_input, _fromIndex) => {
-	return undefined;
+export type ParsedNumber = {
+	format: "dec";
+	value: number;
 };
+
+export const parseNumber: Parser<ParsedNumber> = parseMonad(
+	parseOneOrMore(parseDigit(10)),
+	(digits) => ({
+		format: "dec",
+		value: parseInt(digits.join(""), 10),
+	}),
+);
