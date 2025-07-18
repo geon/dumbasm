6ceaf59f2c15cb3246bdaf5aa418c9b5fba1832a
Victor Widell <victor.widell@divid.se>
2025-07-18 20:08:57 +0200

    Test: parseIdentifier > 123numberPrefix

--- src/parsers/parseIdentifier.ts
+++ src/parsers/parseIdentifier.ts
@@ -3,8 +3,12 @@ import { parseAlternatives } from "./combinators/parseAlternatives.js";
 import { parseDigit } from "./combinators/parseDigit.js";
 import { parseMonad } from "./combinators/parseMonad.js";
 import { parseOneOrMore } from "./combinators/parseOneOrMore.js";
+import { parseSequence } from "./combinators/parseSequence.js";
 
 export const parseIdentifier = parseMonad(
-	parseOneOrMore(parseAlternatives([parseAlpha, parseDigit(10)])),
-	(parsed) => parsed.join(""),
+	parseSequence([
+		parseAlpha,
+		parseOneOrMore(parseAlternatives([parseAlpha, parseDigit(10)])),
+	]),
+	(parsed) => parsed.flat().join(""),
 );
