f88367991a53e25842f19596bcf6b667f98323ce
Victor Widell <victor.widell@divid.se>
2025-07-18 19:08:05 +0200

    Test: parseAlpha >

---
+++ src/parsers/combinators/tests/parseAlpha.test.ts
@@ -0,0 +1,47 @@
+import { parseAlpha } from "../parseAlpha.js";
+import { testExamples } from "./testExamples.js";
+
+testExamples("parseAlpha", [
+	{
+		name: "no match",
+		parser: parseAlpha,
+		input: "-",
+		result: undefined,
+	},
+	{
+		name: "match low lowercase",
+		parser: parseAlpha,
+		input: "a",
+		result: {
+			consumed: 1,
+			parsed: "a",
+		},
+	},
+	{
+		name: "match high lowercase",
+		parser: parseAlpha,
+		input: "z",
+		result: {
+			consumed: 1,
+			parsed: "z",
+		},
+	},
+	{
+		name: "match low uppercase",
+		parser: parseAlpha,
+		input: "A",
+		result: {
+			consumed: 1,
+			parsed: "A",
+		},
+	},
+	{
+		name: "match high uppercase",
+		parser: parseAlpha,
+		input: "Z",
+		result: {
+			consumed: 1,
+			parsed: "Z",
+		},
+	},
+]);
