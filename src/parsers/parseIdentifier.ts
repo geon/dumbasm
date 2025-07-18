8475f1c52cd1969f541878d08975a67c3c7e6f92
Victor Widell <victor.widell@divid.se>
2025-07-18 20:06:47 +0200

    Test: parseIdentifier > numberSuffix123

--- src/parsers/parseIdentifier.ts
+++ src/parsers/parseIdentifier.ts
@@ -1,4 +1,6 @@
 import { parseAlpha } from "./combinators/parseAlpha.js";
+import { parseAlternatives } from "./combinators/parseAlternatives.js";
+import { parseDigit } from "./combinators/parseDigit.js";
 import { parseMonad } from "./combinators/parseMonad.js";
 import { parseOneOrMore } from "./combinators/parseSome.js";
 import { parseWithErrorMessage } from "./combinators/parseWithErrorMessage.js";
@@ -6,8 +8,7 @@ import { parseWithErrorMessage } from "./combinators/parseWithErrorMessage.js";
 export const parseIdentifier = parseWithErrorMessage<string>(
 	"Expected an identifier",
 	parseMonad(
-		//
-		parseOneOrMore(parseAlpha),
+		parseOneOrMore(parseAlternatives([parseAlpha, parseDigit(10)])),
 		(parsed, { result }) => result(parsed.join("")),
 	),
 );
