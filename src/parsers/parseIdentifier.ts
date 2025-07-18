8c8b12bc47cc7e18241877b5feff5b07d03265f0
Victor Widell <victor.widell@divid.se>
2025-07-18 20:04:07 +0200

    Test: parseIdentifier > hello world

--- src/parsers/parseIdentifier.ts
+++ src/parsers/parseIdentifier.ts
@@ -1,3 +1,8 @@
 import { parseAlpha } from "./combinators/parseAlpha.js";
+import { parseMonad } from "./combinators/parseMonad.js";
+import { parseOneOrMore } from "./combinators/parseOneOrMore.js";
 
-export const parseIdentifier = parseAlpha;
+export const parseIdentifier = parseMonad(
+	parseOneOrMore(parseAlpha),
+	(parsed) => parsed.join(""),
+);
