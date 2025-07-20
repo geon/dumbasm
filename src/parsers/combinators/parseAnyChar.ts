2fb531bd308c320b0d2410ac0ddf1468e6c72d3d
Victor Widell <victor.widell@divid.se>
2025-07-20 16:58:37 +0200

    Test: parseAnyChar > match

--- src/parsers/combinators/parseAnyChar.ts
+++ src/parsers/combinators/parseAnyChar.ts
@@ -1,5 +1,13 @@
 import type { Parser } from "./Parser.js";
 
-export const parseAnyChar: Parser<string> = (_input, _fromIndex) => {
-	return undefined;
+export const parseAnyChar: Parser<string> = (input, fromIndex) => {
+	const parsed = input[fromIndex];
+	if (parsed === undefined) {
+		return undefined;
+	}
+
+	return {
+		consumed: 1,
+		parsed,
+	};
 };
