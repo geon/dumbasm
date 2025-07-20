9e8ca0f5d80b0dd037876c70b7fd2696e6011962
Victor Widell <victor.widell@divid.se>
2025-07-20 17:21:23 +0200

    Test: parseMonad > no match

---
+++ src/parsers/combinators/parseMonad.ts
@@ -0,0 +1,10 @@
+import type { Parser } from "./Parser.js";
+
+export function parseMonad<T, T2>(
+	_parser: Parser<T>,
+	_transform: (parsed: T) => T2,
+): Parser<T2> {
+	return (_input, _fromIndex) => {
+		return undefined;
+	};
+}
