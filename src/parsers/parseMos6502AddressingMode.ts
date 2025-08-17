06854f10643f1bba7c984861f294c3c1cef22b88
Victor Widell <victor.widell@divid.se>
2025-08-17 13:37:21 +0200

    Test: parseMos6502AddressingMode >

---
+++ src/parsers/parseMos6502AddressingMode.ts
@@ -0,0 +1,160 @@
+import {
+	type Mos6502AddressingMode,
+	type Mos6502Operand,
+} from "../mos6502Instructions.js";
+import { parseAlternatives } from "./combinators/parseAlternatives.js";
+import { parseChar } from "./combinators/parseChar.js";
+import { parseMonad } from "./combinators/parseMonad.js";
+import type { Parser } from "./combinators/Parser.js";
+import { parseSequence } from "./combinators/parseSequence.js";
+import { parseString } from "./combinators/parseString.js";
+import { parseIdentifier } from "./parseIdentifier.js";
+import { parseNumber, type ParsedNumber } from "./parseNumber.js";
+
+export type ParsableMos6502AddressingMode = Exclude<
+	Mos6502AddressingMode,
+	// "zeropage" is just absolute with smaller values.
+	"zeropage,X" | "zeropage,Y" | "zeropage"
+>;
+
+type Operand =
+	| {
+			type: "number";
+			number: ParsedNumber;
+	  }
+	| {
+			type: "identifier";
+			identifier: string;
+	  };
+
+type OperandForAddressingMode<
+	AddressingMode extends ParsableMos6502AddressingMode,
+> = Mos6502Operand<AddressingMode, Operand>;
+
+export type ParsedMos6502AddressingMode<
+	AddressingMode extends ParsableMos6502AddressingMode,
+> = {
+	addressingMode: AddressingMode;
+	operand: OperandForAddressingMode<AddressingMode>;
+};
+
+const parseAccumulatorAddressingMode: Parser<
+	ParsedMos6502AddressingMode<"accumulator">
+> = parseMonad(parseIdentifier, (identifier) => {
+	if (identifier !== "A") {
+		return undefined;
+	}
+
+	return {
+		addressingMode: "accumulator",
+		operand: undefined,
+	};
+});
+
+const parseOperand: Parser<Operand> = parseAlternatives([
+	parseMonad(
+		parseNumber,
+		(number): Operand => ({
+			type: "number",
+			number,
+		}),
+	),
+	parseMonad(
+		parseIdentifier,
+		(identifier): Operand => ({
+			type: "identifier",
+			identifier,
+		}),
+	),
+]);
+
+const parseImmediateAddressingMode: Parser<
+	ParsedMos6502AddressingMode<"immediate">
+> = parseMonad(
+	parseSequence([parseChar("#"), parseOperand]),
+	([, operand]) => ({
+		addressingMode: "immediate",
+		operand,
+	}),
+);
+
+function parseNumberOperandAddressingMode<
+	AddressingMode extends "absolute" | "relative",
+>(
+	addressingMode: AddressingMode,
+): Parser<ParsedMos6502AddressingMode<"absolute" | "relative">> {
+	return parseMonad(parseOperand, (operand) => {
+		return {
+			addressingMode,
+			operand,
+		};
+	});
+}
+
+function parseIndexedAddressingMode<
+	AddressingMode extends "absolute,X" | "absolute,Y",
+>(
+	addressingMode: AddressingMode,
+): Parser<ParsedMos6502AddressingMode<"absolute,X" | "absolute,Y">> {
+	return parseMonad(
+		parseSequence([
+			parseOperand,
+			parseChar(","),
+			parseChar(addressingMode.slice(-1)),
+		]),
+		([operand]) => {
+			return {
+				addressingMode,
+				operand,
+			};
+		},
+	);
+}
+
+const parseIndirectAddressingMode: Parser<
+	ParsedMos6502AddressingMode<"indirect">
+> = parseMonad(parseOperand, (operand) => ({
+	addressingMode: "indirect",
+	operand,
+}));
+
+const parseIndirectXAddressingMode: Parser<
+	ParsedMos6502AddressingMode<"(indirect,X)">
+> = parseMonad(
+	parseSequence([parseChar("("), parseOperand, parseString(",X)")]),
+	([, operand]) => ({
+		addressingMode: "(indirect,X)",
+		operand,
+	}),
+);
+
+const parseIndirectYAddressingMode: Parser<
+	ParsedMos6502AddressingMode<"(indirect),Y">
+> = parseMonad(
+	parseSequence([parseChar("("), parseOperand, parseString("),Y")]),
+	([, operand]) => ({
+		addressingMode: "(indirect),Y",
+		operand,
+	}),
+);
+
+// TODO: Replace addressing mode parsers with addressing mode syntax parsers. The
+// actual addressing mode is determined by the assembler. absolute can be used interchangably with zeropage for the zero page, etc.
+export const mos6502AddressingModeParsers = {
+	accumulator: parseAccumulatorAddressingMode,
+	immediate: parseImmediateAddressingMode,
+	absolute: parseNumberOperandAddressingMode("absolute"),
+	relative: parseNumberOperandAddressingMode("relative"),
+	"absolute,X": parseIndexedAddressingMode("absolute,X"),
+	"absolute,Y": parseIndexedAddressingMode("absolute,Y"),
+	indirect: parseIndirectAddressingMode,
+	"(indirect,X)": parseIndirectXAddressingMode,
+	"(indirect),Y": parseIndirectYAddressingMode,
+} as const satisfies Record<
+	Exclude<
+		ParsableMos6502AddressingMode,
+		// "implied" can't beparsed because it is *implied*.
+		"implied"
+	>,
+	Parser<ParsedMos6502AddressingMode<ParsableMos6502AddressingMode>>
+>;
---
+++ src/parsers/tests/parseMos6502AddressingMode.test.ts
@@ -0,0 +1,78 @@
+import { testExamples } from "../combinators/tests/testExamples.js";
+import {
+	mos6502AddressingModeParsers,
+	type ParsableMos6502AddressingMode,
+	type ParsedMos6502AddressingMode,
+} from "../parseMos6502AddressingMode.js";
+
+testExamples<ParsedMos6502AddressingMode<ParsableMos6502AddressingMode>>(
+	"parseMos6502AddressingMode",
+	[
+		{
+			input: "A",
+			parser: mos6502AddressingModeParsers["accumulator"],
+			result: {
+				consumed: 1,
+				parsed: {
+					addressingMode: "accumulator",
+					operand: undefined,
+				},
+			},
+		},
+		{
+			input: "#123",
+			parser: mos6502AddressingModeParsers["immediate"],
+			result: {
+				consumed: 4,
+				parsed: {
+					addressingMode: "immediate",
+					operand: { type: "number", number: { format: "dec", value: 123 } },
+				},
+			},
+		},
+		{
+			input: "$abcd,X",
+			parser: mos6502AddressingModeParsers["absolute,X"],
+			result: {
+				consumed: 7,
+				parsed: {
+					addressingMode: "absolute,X",
+					operand: { type: "number", number: { format: "hex", value: 0xabcd } },
+				},
+			},
+		},
+		{
+			input: "$abcd",
+			parser: mos6502AddressingModeParsers["indirect"],
+			result: {
+				consumed: 5,
+				parsed: {
+					addressingMode: "indirect",
+					operand: { type: "number", number: { format: "hex", value: 0xabcd } },
+				},
+			},
+		},
+		{
+			input: "($abcd,X)",
+			parser: mos6502AddressingModeParsers["(indirect,X)"],
+			result: {
+				consumed: 9,
+				parsed: {
+					addressingMode: "(indirect,X)",
+					operand: { type: "number", number: { format: "hex", value: 0xabcd } },
+				},
+			},
+		},
+		{
+			input: "($abcd),Y",
+			parser: mos6502AddressingModeParsers["(indirect),Y"],
+			result: {
+				consumed: 9,
+				parsed: {
+					addressingMode: "(indirect),Y",
+					operand: { type: "number", number: { format: "hex", value: 0xabcd } },
+				},
+			},
+		},
+	],
+);
