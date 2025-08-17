ba7598cda8446deef563f67607306004d8c46370
Victor Widell <victor.widell@divid.se>
2025-08-17 11:22:15 +0200

    Test: parseSequence > empty

---
+++ src/parsers/combinators/parseSequence.ts
@@ -0,0 +1,27 @@
+import type { Parser } from "./Parser.js";
+
+type Tail<T extends readonly unknown[]> = T extends readonly [
+	unknown,
+	...infer Rest,
+]
+	? Rest
+	: readonly [];
+
+type SequenceResults<TParsers extends readonly Parser<unknown>[]> =
+	TParsers extends readonly []
+		? []
+		: [
+				Exclude<ReturnType<TParsers[0]>, undefined>["parsed"],
+				...SequenceResults<Tail<TParsers>>,
+			];
+
+export function parseSequence<const Parsers extends readonly Parser<unknown>[]>(
+	_parsers: Parsers,
+): Parser<SequenceResults<Parsers>> {
+	return (_input, _fromIndex) => {
+		return {
+			consumed: 0,
+			parsed: [] as any,
+		};
+	};
+}
