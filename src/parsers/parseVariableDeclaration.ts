e36c2b633152189c9a78ac309b7db180ae06e9ba
Victor Widell <victor.widell@divid.se>
2025-08-17 14:24:57 +0200

    Test: parseVariableDeclaration > regA uint8 myVariable

--- src/parsers/parseVariableDeclaration.ts
+++ src/parsers/parseVariableDeclaration.ts
@@ -1,11 +1,15 @@
 import { parseMonad } from "./combinators/parseMonad.js";
 import { parseOneOrMore } from "./combinators/parseOneOrMore.js";
+import { parseOptional } from "./combinators/parseOptional.js";
 import type { Parser } from "./combinators/Parser.js";
 import { parseSequence } from "./combinators/parseSequence.js";
 import { parseWhitespace } from "./combinators/parseWhitespace.js";
 import { parseIdentifier } from "./parseIdentifier.js";
 import { parseType, type Type } from "./parseType.js";
-import type { VariableLocation } from "./parseVariableLocation.js";
+import {
+	parseVariableLocation,
+	type VariableLocation,
+} from "./parseVariableLocation.js";
 
 export type ParsedVariableDeclaration = Readonly<{
 	type: Type;
@@ -16,14 +20,17 @@ export type ParsedVariableDeclaration = Readonly<{
 export const parseVariableDeclaration: Parser<ParsedVariableDeclaration> =
 	parseMonad(
 		parseSequence([
+			parseOptional(
+				parseSequence([parseVariableLocation, parseOneOrMore(parseWhitespace)]),
+			),
 			parseType,
 			parseOneOrMore(parseWhitespace),
 			parseIdentifier,
 		]),
-		([type, , name]): ParsedVariableDeclaration => {
+		([loc, type, , name]): ParsedVariableDeclaration => {
 			return {
 				type,
-				location: undefined,
+				location: loc?.[0],
 				name,
 			};
 		},
--- src/parsers/tests/parseVariableDeclaration.test.ts
+++ src/parsers/tests/parseVariableDeclaration.test.ts
@@ -19,4 +19,16 @@ testExamples("parseVariableDeclaration", [
 			},
 		},
 	},
+	{
+		input: "regA uint8 myVariable",
+		parser: parseVariableDeclaration,
+		result: {
+			consumed: 21,
+			parsed: {
+				location: "regA",
+				type: "uint8",
+				name: "myVariable",
+			},
+		},
+	},
 ]);
