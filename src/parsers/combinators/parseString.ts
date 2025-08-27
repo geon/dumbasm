a32f152bdc757c46b80e4a4e8ef5393657286fbc
Victor Widell <victor.widell@divid.se>
2025-08-27 21:46:14 +0200

    Test: parseString > case insensitive

--- src/parsers/combinators/parseString.ts
+++ src/parsers/combinators/parseString.ts
@@ -1,4 +1,4 @@
-import { parseChar } from "./parseChar.js";
+import { parseChar, parseCharCaseInsensitive } from "./parseChar.js";
 import { parseMonad } from "./parseMonad.js";
 import type { Parser } from "./Parser.js";
 import { parseSequence } from "./parseSequence.js";
@@ -13,5 +13,14 @@ export function parseString<TString extends string>(
 			parseSequence(string.split("").map(parseChar)),
 			(_, { result }) => result(string),
 		),
+	);
+}
+
+export function parseStringCaseInsensitive<TString extends string>(
+	string: TString,
+): Parser<TString> {
+	return parseMonad(
+		parseSequence(string.split("").map(parseCharCaseInsensitive)),
+		(_, { result }) => result(string),
 	);
 }
