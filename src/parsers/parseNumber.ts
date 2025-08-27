3b9dd780968c7f98296775dc34066f693b8187c4
Victor Widell <victor.widell@divid.se>
2025-08-27 21:28:31 +0200

    Test: parseNumber > $dead

--- src/parsers/parseNumber.ts
+++ src/parsers/parseNumber.ts
@@ -1,12 +1,18 @@
+import { parseAlternatives } from "./combinators/parseAlternatives.js";
+import { parseChar } from "./combinators/parseChar.js";
 import { parseDigit } from "./combinators/parseDigit.js";
 import { parseMonad } from "./combinators/parseMonad.js";
 import { parseOneOrMore } from "./combinators/parseSome.js";
 import type { Parser } from "./combinators/Parser.js";
 import { parseWithErrorMessage } from "./combinators/parseWithErrorMessage.js";
+import { parseSequenceIndex } from "./combinators/parseSequenceIndex.js";
 
 export const parseNumber: Parser<number> = parseWithErrorMessage(
 	"Expected a number.",
-	parseDigits(10),
+	parseAlternatives([
+		parseSequenceIndex(1, [parseChar("$"), parseDigits(16)]),
+		parseDigits(10),
+	]),
 );
 
 function parseDigits(base: number): Parser<number> {
