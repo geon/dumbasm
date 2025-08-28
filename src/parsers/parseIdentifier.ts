5ab5b8d63eb4fe816c776a78fd323db1f56ee502
Victor Widell <victor.widell@divid.se>
2025-08-29 01:15:07 +0200

    Test: parseIdentifier > 123numberPrefix

--- src/parsers/parseIdentifier.ts
+++ src/parsers/parseIdentifier.ts
@@ -2,13 +2,17 @@ import { parseAlpha } from "./combinators/parseAlpha.js";
 import { parseAlternatives } from "./combinators/parseAlternatives.js";
 import { parseDigit } from "./combinators/parseDigit.js";
 import { parseMonad } from "./combinators/parseMonad.js";
+import { parseSequence } from "./combinators/parseSequence.js";
 import { parseOneOrMore } from "./combinators/parseSome.js";
 import { parseWithErrorMessage } from "./combinators/parseWithErrorMessage.js";
 
 export const parseIdentifier = parseWithErrorMessage<string>(
 	"Expected an identifier",
 	parseMonad(
-		parseOneOrMore(parseAlternatives([parseAlpha, parseDigit(10)])),
-		(parsed, { result }) => result(parsed.join("")),
+		parseSequence([
+			parseAlpha,
+			parseOneOrMore(parseAlternatives([parseAlpha, parseDigit(10)])),
+		]),
+		(parsed, { result }) => result(parsed.flat().join("")),
 	),
 );
