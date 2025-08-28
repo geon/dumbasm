f9fa971046a8c8177afd2b554f95152a3d25e157
Victor Widell <victor.widell@divid.se>
2025-08-29 00:53:35 +0200

    Test: parseNewline >

---
+++ src/parsers/combinators/tests/parseNewline.test.ts
@@ -0,0 +1,21 @@
+import { parseNewline } from "../parseNewline.js";
+import { failParsing } from "../Parser.js";
+import { testExamples } from "./testExamples.js";
+
+testExamples("parseNewline", [
+	{
+		name: "no match",
+		parser: parseNewline,
+		input: "-",
+		result: failParsing(),
+	},
+	{
+		name: "match",
+		parser: parseNewline,
+		input: "\n",
+		result: {
+			consumed: 1,
+			parsed: "\n",
+		},
+	},
+]);
