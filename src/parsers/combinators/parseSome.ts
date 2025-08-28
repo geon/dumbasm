e7ed0fc13e156c7a387070f21109fc389331fb92
Victor Widell <victor.widell@divid.se>
2025-08-29 01:04:51 +0200

    Test: parseSome > no match

--- src/parsers/combinators/parseSome.ts
+++ src/parsers/combinators/parseSome.ts
@@ -1,4 +1,4 @@
-import { parsingFailed, type Parser } from "./Parser.js";
+import { failParsing, parsingFailed, type Parser } from "./Parser.js";
 
 export function parseOneOrMore<T>(parser: Parser<T>): Parser<readonly T[]> {
 	return parseSome(parser, 1);
@@ -8,7 +8,7 @@ export function parseZeroOrMore<T>(parser: Parser<T>): Parser<readonly T[]> {
 	return parseSome(parser, 0);
 }
 
-function parseSome<T>(parser: Parser<T>, _min: number): Parser<readonly T[]> {
+function parseSome<T>(parser: Parser<T>, min: number): Parser<readonly T[]> {
 	return (input, fromIndex) => {
 		let consumed = 0;
 		const parsed = [];
@@ -19,6 +19,10 @@ function parseSome<T>(parser: Parser<T>, _min: number): Parser<readonly T[]> {
 			}
 			consumed += parseResult.consumed;
 			parsed.push(parseResult.parsed);
+		}
+
+		if (parsed.length < min) {
+			return failParsing();
 		}
 
 		return {
--- src/parsers/combinators/tests/parseSome.test.ts
+++ src/parsers/combinators/tests/parseSome.test.ts
@@ -1,6 +1,7 @@
 import { parseOneOrMore, parseZeroOrMore } from "../parseSome.js";
 import { parseChar } from "../parseChar.js";
 import { testExamples } from "./testExamples.js";
+import { failParsing } from "../Parser.js";
 
 testExamples<readonly string[]>("parseSome", [
 	{
@@ -19,6 +20,21 @@ testExamples<readonly string[]>("parseSome", [
 		result: {
 			consumed: 2,
 			parsed: ["a", "a"],
+		},
+	},
+	{
+		name: "parseOneOrMore no match",
+		parser: parseOneOrMore(parseChar("b")),
+		input: "abc",
+		result: failParsing(),
+	},
+	{
+		name: "parseZeroOrMore no match",
+		parser: parseZeroOrMore(parseChar("b")),
+		input: "abc",
+		result: {
+			consumed: 0,
+			parsed: [],
 		},
 	},
 ]);
