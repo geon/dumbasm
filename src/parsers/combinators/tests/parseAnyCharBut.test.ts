ff1771628e96768e2e03d5afeda41e8f8e9d0329
Victor Widell <victor.widell@divid.se>
2025-08-29 00:52:55 +0200

    Test: parseAnyCharBut >

---
+++ src/parsers/combinators/tests/parseAnyCharBut.test.ts
@@ -0,0 +1,21 @@
+import { parseAnyCharBut } from "../parseAnyCharBut.js";
+import { failParsing } from "../Parser.js";
+import { testExamples } from "./testExamples.js";
+
+testExamples("parseAnyCharBut", [
+	{
+		name: "no match",
+		parser: parseAnyCharBut("a"),
+		input: "abc",
+		result: failParsing(),
+	},
+	{
+		name: "match",
+		parser: parseAnyCharBut("b"),
+		input: "abc",
+		result: {
+			consumed: 1,
+			parsed: "a",
+		},
+	},
+]);
