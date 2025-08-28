453bd1ec67c5140a82783cbf58a0527be8e30b35
Victor Widell <victor.widell@divid.se>
2025-08-29 01:04:51 +0200

    Test: parseSome > no match

--- src/parsers/combinators/parseSome.ts
+++ src/parsers/combinators/parseSome.ts
@@ -1,4 +1,10 @@
-import { createParseResult, parsingFailed, type Parser } from "./Parser.js";
+import { indent } from "../../fp/indent.js";
+import {
+	createParseError,
+	createParseResult,
+	parsingFailed,
+	type Parser,
+} from "./Parser.js";
 
 export function parseOneOrMore<T>(parser: Parser<T>): Parser<readonly T[]> {
 	return parseSome(parser, 1);
@@ -8,17 +14,28 @@ export function parseZeroOrMore<T>(parser: Parser<T>): Parser<readonly T[]> {
 	return parseSome(parser, 0);
 }
 
-function parseSome<T>(parser: Parser<T>, _min: number): Parser<readonly T[]> {
+function parseSome<T>(parser: Parser<T>, min: number): Parser<readonly T[]> {
 	return (input, fromIndex) => {
 		let consumed = 0;
 		const parsed = [];
+		let errorMessage;
 		for (;;) {
 			const parseResult = parser(input, fromIndex + consumed);
 			if (parsingFailed(parseResult)) {
+				errorMessage = parseResult.message;
 				break;
 			}
 			consumed += parseResult.consumed;
 			parsed.push(parseResult.parsed);
+		}
+
+		if (parsed.length < min) {
+			return createParseError(
+				fromIndex,
+				min === 1
+					? errorMessage
+					: `Found only ${parsed.length} of ${min} repeated matches.\n${indent(errorMessage)}`,
+			);
 		}
 
 		return createParseResult(consumed, parsed);
--- src/parsers/combinators/tests/parseSome.test.ts
+++ src/parsers/combinators/tests/parseSome.test.ts
@@ -1,7 +1,7 @@
 import { parseOneOrMore, parseZeroOrMore } from "../parseSome.js";
 import { parseChar } from "../parseChar.js";
 import { testExamples } from "./testExamples.js";
-import { createParseResult } from "../Parser.js";
+import { createParseError, createParseResult } from "../Parser.js";
 
 testExamples<readonly string[]>("parseSome", [
 	{
@@ -15,5 +15,17 @@ testExamples<readonly string[]>("parseSome", [
 		parser: parseZeroOrMore(parseChar("a")),
 		input: "aabc",
 		result: createParseResult(2, ["a", "a"]),
+	},
+	{
+		name: "parseOneOrMore no match",
+		parser: parseOneOrMore(parseChar("b")),
+		input: "abc",
+		result: createParseError(0, 'Expected char "b".'),
+	},
+	{
+		name: "parseZeroOrMore no match",
+		parser: parseZeroOrMore(parseChar("b")),
+		input: "abc",
+		result: createParseResult(0, []),
 	},
 ]);
