00fe43043e217f2f1dbbc6a48a3dcbcd3af838b1
Victor Widell <victor.widell@divid.se>
2025-08-29 18:43:53 +0200

    Refactor. Enabled parsing of dumbasm files.

--- src/parsers/parseFile.ts
+++ src/parsers/parseFile.ts
@@ -7,8 +7,9 @@ import { type ParserArgs, type ParseResult } from "./combinators/Parser.js";
 import { parseOneOrMore } from "./combinators/parseSome.js";
 import { parseWithErrorMessage } from "./combinators/parseWithErrorMessage.js";
 import { parseAsmLine, type AsmFragment } from "./parseAsmLine.js";
+import { parseDumbasmLine, type DumbasmFragment } from "./parseDumbasmLine.js";
 
-export type ParsedFile = readonly AsmFragment[];
+export type ParsedFile = readonly (AsmFragment | DumbasmFragment)[];
 
 export function parseFile(...args: ParserArgs): ParseResult<ParsedFile> {
 	return parseWithErrorMessage(
@@ -18,14 +19,16 @@ export function parseFile(...args: ParserArgs): ParseResult<ParsedFile> {
 				parseOneOrMore(
 					parseKeyed({
 						asm: parseAsmLine,
+						dumbasm: parseDumbasmLine,
 						newline: parseNewline,
 					}),
 				),
 				(matches, { result }) =>
 					result(
 						matches
-							.filter((match) => match.type === "asm")
-							.flatMap((match) => match.value),
+							.filter((match) => match.type !== "newline")
+							.map((match) => match.value)
+							.flat(),
 					),
 			),
 			parseMonad(parseEof, (_, { result }) => result([])),
