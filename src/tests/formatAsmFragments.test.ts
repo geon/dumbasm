ebe558ba8209d2d7dbdd02d33bde4c5f331ec382
Victor Widell <victor.widell@divid.se>
2025-08-29 16:59:47 +0200

    Test: formatAsmFragments > helloWorld

---
+++ src/tests/formatAsmFragments.test.ts
@@ -0,0 +1,39 @@
+import { suite, test, expect } from "vitest";
+import { formatAsmFragments } from "../formatAsmFragments";
+import { parseFile } from "../parsers/parseFile";
+import { parsingFailed } from "../parsers/combinators/Parser";
+import { asmSamples } from "../parsers/tests/asm-samples";
+
+testExamples("formatAsmFragments", [
+	{
+		name: "hello world",
+		input: asmSamples.helloWorld,
+	},
+]);
+
+function parse(fileContents: string) {
+	const parsed = parseFile(fileContents, 0);
+	if (parsingFailed(parsed)) {
+		throw new Error("Parsing failed.");
+	}
+
+	const asmFragments = parsed.parsed;
+
+	return asmFragments;
+}
+
+export function testExamples(
+	suiteName: string,
+	examples: readonly {
+		readonly name: string;
+		readonly input: string;
+	}[],
+) {
+	suite(suiteName, () => {
+		for (const example of examples) {
+			test(example.name, () => {
+				expect(formatAsmFragments(parse(example.input))).toMatchSnapshot();
+			});
+		}
+	});
+}
