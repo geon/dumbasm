0754dabf6f11e8b83a331d5ed2b7800e7c4ca166
Victor Widell <victor.widell@divid.se>
2025-08-29 16:03:42 +0200

    Test: parseSequenceIndex > match second

--- src/parsers/combinators/tests/parseSequenceIndex.test.ts
+++ src/parsers/combinators/tests/parseSequenceIndex.test.ts
@@ -1,8 +1,22 @@
 import { suite, test, expect } from "vitest";
+import { parseChar } from "../parseChar.js";
 import { parseSequenceIndex } from "../parseSequenceIndex.js";
+import { testExamples } from "./testExamples.js";
 
 suite("parseSequenceIndex", () => {
 	test("out of bounds", () => {
 		expect(() => parseSequenceIndex(1, [])).toThrow();
 	});
 });
+
+testExamples("parseSequenceIndex", [
+	{
+		name: "match second",
+		parser: parseSequenceIndex(1, [parseChar("a"), parseChar("b")]),
+		input: "abc",
+		result: {
+			consumed: 2,
+			parsed: "b",
+		},
+	},
+]);
