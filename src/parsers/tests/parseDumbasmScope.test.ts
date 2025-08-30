b82cefa164c851e921be004fab1908a24f030963
Victor Widell <victor.widell@divid.se>
2025-08-30 13:14:14 +0200

    Test: parseDumbasmScope > {}

--- src/parsers/tests/parseDumbasmScope.test.ts
+++ src/parsers/tests/parseDumbasmScope.test.ts
@@ -1,11 +1,16 @@
 import { parseDumbasmScope } from "../parseDumbasmScope.js";
 import { testExamples } from "../combinators/tests/testExamples.js";
-import { createParseError } from "../combinators/Parser.js";
+import { createParseError, createParseResult } from "../combinators/Parser.js";
 
 testExamples("parseDumbasmScope", [
 	{
 		input: "360 noscope",
 		parser: parseDumbasmScope,
 		result: createParseError(0, "Expected scope."),
+	},
+	{
+		input: "{}",
+		parser: parseDumbasmScope,
+		result: createParseResult(2, []),
 	},
 ]);
