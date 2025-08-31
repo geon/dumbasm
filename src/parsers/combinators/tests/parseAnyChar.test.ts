9bedae1f380828c09853eab6539ed7c848650638
Victor Widell <victor.widell@divid.se>
2025-08-31 22:43:13 +0200

    Test: parseAnyChar > no match

---
+++ src/parsers/combinators/tests/parseAnyChar.test.ts
@@ -0,0 +1,12 @@
+import { parseAnyChar } from "../parseAnyChar.js";
+import { createParseError } from "../Parser.js";
+import { testExamples } from "./testExamples.js";
+
+testExamples("parseAnyChar", [
+	{
+		name: "no match",
+		parser: parseAnyChar,
+		input: "",
+		result: createParseError(0, "Unexpectedly reached end of file."),
+	},
+]);
