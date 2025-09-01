b84f63c745efd333732d2b00a450da43469896d4
Victor Widell <victor.widell@divid.se>
2025-09-01 14:20:47 +0200

    Test: parseIdentifier > hello world

--- src/parsers/tests/parseIdentifier.test.ts
+++ src/parsers/tests/parseIdentifier.test.ts
@@ -1,11 +1,16 @@
 import { parseIdentifier } from "../parseIdentifier.js";
 import { testExamples } from "../combinators/tests/testExamples.js";
-import { createParseError } from "../combinators/Parser.js";
+import { createParseError, createParseResult } from "../combinators/Parser.js";
 
 testExamples("parseIdentifier", [
 	{
 		input: "#not an identifier",
 		parser: parseIdentifier,
 		result: createParseError(0, "Expected an identifier"),
+	},
+	{
+		input: "hello world",
+		parser: parseIdentifier,
+		result: createParseResult(5, "hello"),
 	},
 ]);
