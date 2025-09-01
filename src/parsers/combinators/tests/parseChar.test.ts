64b0116f6e33cb8c4da44aa93d656edab64e1efb
Victor Widell <victor.widell@divid.se>
2025-09-01 09:39:00 +0200

    Test: parseChar > no match

--- src/parsers/combinators/tests/parseChar.test.ts
+++ src/parsers/combinators/tests/parseChar.test.ts
@@ -1,8 +1,19 @@
 import { suite, test, expect } from "vitest";
 import { parseChar } from "../parseChar.js";
+import { testExamples } from "./testExamples.js";
+import { createParseError } from "../Parser.js";
 
 suite("parseChar", () => {
 	test("too long", () => {
 		expect(() => parseChar("too long")).toThrow();
 	});
 });
+
+testExamples("parseChar", [
+	{
+		name: "no match",
+		parser: parseChar("b"),
+		input: "abc",
+		result: createParseError(0, 'Expected char "b".'),
+	},
+]);
