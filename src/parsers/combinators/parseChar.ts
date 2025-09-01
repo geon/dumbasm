29fa0925b0ae85c38f21a0cc75d3110cc2661b24
Victor Widell <victor.widell@divid.se>
2025-09-01 09:39:00 +0200

    Test: parseChar > no match

--- src/parsers/combinators/parseChar.ts
+++ src/parsers/combinators/parseChar.ts
@@ -1,9 +1,10 @@
-import { type Parser } from "./Parser.js";
+import { createParseError, type Parser } from "./Parser.js";
 
 export function parseChar<Char extends string>(char: Char): Parser<Char> {
 	if (char.length !== 1) {
 		throw new Error(`Not a char: "${char}"`);
 	}
 
-	throw new Error("Not implemented.");
+	return (_, fromIndex) =>
+		createParseError(fromIndex, `Expected char ${JSON.stringify(char)}.`);
 }
