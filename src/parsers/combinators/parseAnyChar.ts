799019cd0dce8be3913f2de1355e06813cf7e43d
Victor Widell <victor.widell@divid.se>
2025-09-01 11:25:04 +0200

    Test: parseAnyChar > match

--- src/parsers/combinators/parseAnyChar.ts
+++ src/parsers/combinators/parseAnyChar.ts
@@ -1,5 +1,10 @@
-import { createParseError, type Parser } from "./Parser.js";
+import { createParseError, createParseResult, type Parser } from "./Parser.js";
 
-export const parseAnyChar: Parser<string> = (_input, fromIndex) => {
-	return createParseError(fromIndex, "Unexpectedly reached end of file.");
+export const parseAnyChar: Parser<string> = (input, fromIndex) => {
+	const parsed = input[fromIndex];
+	if (parsed === undefined) {
+		return createParseError(fromIndex, "Unexpectedly reached end of file.");
+	}
+
+	return createParseResult(1, parsed);
 };
