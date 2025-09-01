602bdf0bd973fa1bdd0073749b859d77e42850fb
Victor Widell <victor.widell@divid.se>
2025-09-01 14:02:41 +0200

    Test: parseNumber > 123

--- src/parsers/parseNumber.ts
+++ src/parsers/parseNumber.ts
@@ -1,4 +1,16 @@
-import { createParseError, type Parser } from "./combinators/Parser.js";
+import { parseDigit } from "./combinators/parseDigit.js";
+import { parseMonad } from "./combinators/parseMonad.js";
+import { parseOneOrMore } from "./combinators/parseSome.js";
+import type { Parser } from "./combinators/Parser.js";
+import { parseWithErrorMessage } from "./combinators/parseWithErrorMessage.js";
 
-export const parseNumber: Parser<number> = (_, fromIndex) =>
-	createParseError(fromIndex, "Expected a number.");
+export const parseNumber: Parser<number> = parseWithErrorMessage(
+	"Expected a number.",
+	parseDigits(10),
+);
+
+function parseDigits(base: number): Parser<number> {
+	return parseMonad(parseOneOrMore(parseDigit(base)), (digits, { result }) =>
+		result(parseInt(digits.join(""), base)),
+	);
+}
