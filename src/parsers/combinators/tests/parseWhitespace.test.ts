882a8e7f1ee9dae331247392630f149ec610889a
Victor Widell <victor.widell@divid.se>
2025-09-01 12:55:29 +0200

    Test: parseWhitespace >

---
+++ src/parsers/combinators/tests/parseWhitespace.test.ts
@@ -0,0 +1,24 @@
+import { createParseError, createParseResult } from "../Parser.js";
+import { parseWhitespace } from "../parseWhitespace.js";
+import { testExamples } from "./testExamples.js";
+
+testExamples("parseWhitespace", [
+	{
+		name: "no match",
+		parser: parseWhitespace,
+		input: "-",
+		result: createParseError(0, "Expected whitespace."),
+	},
+	{
+		name: "match space",
+		parser: parseWhitespace,
+		input: " ",
+		result: createParseResult(1, " "),
+	},
+	{
+		name: "match tab",
+		parser: parseWhitespace,
+		input: "\t",
+		result: createParseResult(1, "\t"),
+	},
+]);
