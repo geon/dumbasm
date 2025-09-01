3e4491649e3ac188755b48c4104f0d66da414672
Victor Widell <victor.widell@divid.se>
2025-09-01 09:53:37 +0200

    Test: parseSequenceIndex > match second

--- src/parsers/combinators/parseSequenceIndex.ts
+++ src/parsers/combinators/parseSequenceIndex.ts
@@ -1,12 +1,19 @@
+import { parseMonad } from "./parseMonad";
 import type { Parser } from "./Parser";
-import { type SequenceResults } from "./parseSequence";
+import { type SequenceResults, parseSequence } from "./parseSequence";
 
 export const parseSequenceIndex = <
 	const ValueIndex extends number,
 	const Parsers extends readonly Parser<unknown>[],
 >(
-	_valueIndex: ValueIndex,
-	_parsers: Parsers,
+	valueIndex: ValueIndex,
+	parsers: Parsers,
 ): Parser<SequenceResults<Parsers>[ValueIndex]> => {
-	throw new Error("valueIndex out of bounds");
+	if (valueIndex >= parsers.length) {
+		throw new Error("valueIndex out of bounds");
+	}
+
+	return parseMonad(parseSequence(parsers), (parsed, { result }) =>
+		result(parsed[valueIndex]),
+	);
 };
