3ba8e413f0bda0ac43aaad1f4b5710789d289ed2
Victor Widell <victor.widell@divid.se>
2025-09-01 15:01:17 +0200

    Test: parseChar > case insensitive

--- src/parsers/combinators/parseChar.ts
+++ src/parsers/combinators/parseChar.ts
@@ -13,3 +13,17 @@ export function parseChar<Char extends string>(char: Char): Parser<Char> {
 			: result(char),
 	);
 }
+
+export function parseCharCaseInsensitive<Char extends string>(
+	char: Char,
+): Parser<Char> {
+	if (char.length !== 1) {
+		throw new Error(`Not a char: "${char}"`);
+	}
+
+	return parseMonad(parseAnyChar, (parsed, { error, result }) =>
+		parsed.toLowerCase() !== char.toLowerCase()
+			? error(`Expected char ${JSON.stringify(char)}.`)
+			: result(char),
+	);
+}
