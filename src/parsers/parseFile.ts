64e53e402ff08139dce6c029dc88813a3bc71a53
Victor Widell <victor.widell@divid.se>
2025-09-02 22:44:26 +0200

    Test: parseFile > \ninc\ntax\n\n

--- src/parsers/parseFile.ts
+++ src/parsers/parseFile.ts
@@ -1,7 +1,10 @@
 import { parseAlternatives } from "./combinators/parseAlternatives.js";
 import { parseEof } from "./combinators/parseEof.js";
+import { parseKeyed } from "./combinators/parseKeyed.js";
 import { parseMonad } from "./combinators/parseMonad.js";
+import { parseNewline } from "./combinators/parseNewline.js";
 import { type ParserArgs, type ParseResult } from "./combinators/Parser.js";
+import { parseOneOrMore } from "./combinators/parseSome.js";
 import { parseWithErrorMessage } from "./combinators/parseWithErrorMessage.js";
 import { parseAsmLine, type AsmFragment } from "./parseAsmLine.js";
 
@@ -11,7 +14,20 @@ export function parseFile(...args: ParserArgs): ParseResult<ParsedFile> {
 	return parseWithErrorMessage(
 		"SYNTAX ERROR",
 		parseAlternatives([
-			parseAsmLine,
+			parseMonad(
+				parseOneOrMore(
+					parseKeyed({
+						asm: parseAsmLine,
+						newline: parseNewline,
+					}),
+				),
+				(matches, { result }) =>
+					result(
+						matches
+							.filter((match) => match.type === "asm")
+							.flatMap((match) => match.value),
+					),
+			),
 			parseMonad(parseEof, (_, { result }) => result([])),
 		]),
 	)(...args);
