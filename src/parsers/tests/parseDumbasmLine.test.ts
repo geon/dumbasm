f6ab67bd19d0f1873c23b3d568b1821034a93aed
Victor Widell <victor.widell@divid.se>
2025-09-11 23:53:23 +0200

    Test: parseDumbasmLine >

---
+++ src/parsers/tests/parseDumbasmLine.test.ts
@@ -0,0 +1,36 @@
+import { parseDumbasmLine, type DumbasmFragment } from "../parseDumbasmLine.js";
+import { testExamples } from "../combinators/tests/testExamples.js";
+import { createParseError, createParseResult } from "../combinators/Parser.js";
+
+testExamples<DumbasmFragment>("parseDumbasmLine", [
+	{
+		input: "nonsense",
+		parser: parseDumbasmLine,
+		result: createParseError(
+			0,
+			'No alternative matched.\n\tvariableDeclaration: Expected string "uint8".',
+		),
+	},
+	{
+		name: "empty line",
+		input: "",
+		parser: parseDumbasmLine,
+		result: createParseError(
+			0,
+			'No alternative matched.\n\tvariableDeclaration: Expected string "uint8".',
+		),
+	},
+	{
+		input: "uint8 myVariable ; Comment",
+		parser: parseDumbasmLine,
+		result: createParseResult(26, {
+			type: "variableDeclaration",
+			value: {
+				type: "uint8",
+				location: undefined,
+				lineNum: 1,
+				name: "myVariable",
+			},
+		}),
+	},
+]);
