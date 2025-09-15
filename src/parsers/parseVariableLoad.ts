0913f77518ca38513697c7436599e4f8a25566a8
Victor Widell <victor.widell@divid.se>
2025-09-15 17:03:11 +0200

    Test: parseVariableLoad > ld myVariable myArray,myIndex

--- src/parsers/parseVariableLoad.ts
+++ src/parsers/parseVariableLoad.ts
@@ -1,14 +1,13 @@
 import type { Mos6502AddressingModeOfInstruction } from "../mos6502Instructions.js";
+import { addressingModeCategoryParsers } from "./addressingModeCategoryParsers.js";
+import { parseKeyed } from "./combinators/parseKeyed.js";
 import { parseMonad } from "./combinators/parseMonad.js";
 import { parseSequence } from "./combinators/parseSequence.js";
 import { parseString } from "./combinators/parseString.js";
 import { parseWhitespace } from "./combinators/parseWhitespace.js";
 import { parseIdentifier } from "./parseIdentifier.js";
 import type { ParsedAddressingModeCategory } from "./parseMos6502Instruction.js";
-import {
-	addressingModeParserByMnemonic,
-	type GetAddressingModeCategory,
-} from "./parseMos6502Instruction.js";
+import { type GetAddressingModeCategory } from "./parseMos6502Instruction.js";
 
 export type ParsedVariableLoad = {
 	readonly variable: string;
@@ -17,15 +16,26 @@ export type ParsedVariableLoad = {
 	>;
 };
 
+const parseAddressingMode = parseKeyed({
+	immediate: addressingModeCategoryParsers.immediate,
+	postIndexedIndirect:
+		addressingModeCategoryParsers.postIndexedIndirect(parseIdentifier),
+	preIndexedIndirect:
+		addressingModeCategoryParsers.preIndexedIndirect(parseIdentifier),
+	indexed: addressingModeCategoryParsers.indexed(parseIdentifier),
+	address: addressingModeCategoryParsers.address,
+});
+
 export const parseVariableLoad = parseMonad(
 	parseSequence([
 		//
 		parseString("ld"),
 		parseWhitespace,
 		parseIdentifier,
-		addressingModeParserByMnemonic["lda"],
+		parseWhitespace,
+		parseAddressingMode,
 	]),
-	([, , to, from], { result }) => {
+	([, , to, , from], { result }) => {
 		return result<ParsedVariableLoad>({
 			variable: to,
 			addressingMode: from as any,
